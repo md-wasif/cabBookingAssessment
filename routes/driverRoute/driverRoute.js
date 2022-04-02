86b54c5b484616e56055e14589a0315665318aec
md-wasif <md.wasif7894@gmail.com>
2022-04-02 15:19:37 +0530

    update availability route

--- routes/driverRoute/driverRoute.js
+++ routes/driverRoute/driverRoute.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.post('/register', driverService.register);
 router.get('/addAddress', driverService.addAddress);
 router.get('/editAddress', driverService.editAddress);
+router.put('/updateAvailability', driverService.updateAvailability);
 
 
 module.exports = router;
